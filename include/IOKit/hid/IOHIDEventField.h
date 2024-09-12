1228982a8467edec86248091e8f0c4fab3692968
yl.han <yl.han@trip.com>
2024-09-12 11:20:40 +0800

    first commit

---
+++ include/IOKit/hid/IOHIDEventField.h
@@ -0,0 +1,134 @@
+#ifndef IOHIDEventField_h
+#define IOHIDEventField_h
+
+// TODO: Update with:
+// https://opensource.apple.com/source/IOHIDFamily/IOHIDFamily-700/IOHIDFamily/IOHIDEventTypes.h.auto.html
+// Note that there is `kIOHIDEventFieldDigitizerOrientationType` that can not be found anywhere
+
+// Keys used to set and get individual event fields.
+typedef NS_ENUM(uint32_t, IOHIDEventField) {
+    // NULL
+    kIOHIDEventFieldIsRelative = IOHIDEventFieldBase(kIOHIDEventTypeNULL),
+    kIOHIDEventFieldIsCollection,
+    kIOHIDEventFieldIsPixelUnits,
+    kIOHIDEventFieldIsCenterOrigin,
+    kIOHIDEventFieldIsBuiltIn,
+
+    // VendorDefined
+    kIOHIDEventFieldVendorDefinedUsagePage = IOHIDEventFieldBase(kIOHIDEventTypeVendorDefined),
+    kIOHIDEventFieldVendorDefinedUsage,
+    kIOHIDEventFieldVendorDefinedVersion,
+    kIOHIDEventFieldVendorDefinedDataLength,
+    kIOHIDEventFieldVendorDefinedData,
+
+    // Button
+    kIOHIDEventFieldButtonMask = IOHIDEventFieldBase(kIOHIDEventTypeButton),
+    kIOHIDEventFieldButtonNumber,
+    kIOHIDEventFieldButtonClickCount,
+    kIOHIDEventFieldButtonPressure,
+
+    // Translation
+    kIOHIDEventFieldTranslationX = IOHIDEventFieldBase(kIOHIDEventTypeTranslation),
+    kIOHIDEventFieldTranslationY,
+    kIOHIDEventFieldTranslationZ,
+
+    // Rotation
+    kIOHIDEventFieldRotationX = IOHIDEventFieldBase(kIOHIDEventTypeRotation),
+    kIOHIDEventFieldRotationY,
+    kIOHIDEventFieldRotationZ,
+
+    // Scroll
+    kIOHIDEventFieldScrollX = IOHIDEventFieldBase(kIOHIDEventTypeScroll),
+    kIOHIDEventFieldScrollY,
+    kIOHIDEventFieldScrollZ,
+    kIOHIDEventFieldScrollIsPixels,
+
+    // Scale
+    kIOHIDEventFieldScaleX = IOHIDEventFieldBase(kIOHIDEventTypeScale),
+    kIOHIDEventFieldScaleY,
+    kIOHIDEventFieldScaleZ,
+
+    // Velocity
+    kIOHIDEventFieldVelocityX = IOHIDEventFieldBase(kIOHIDEventTypeVelocity),
+    kIOHIDEventFieldVelocityY,
+    kIOHIDEventFieldVelocityZ,
+
+    // Accelerometer
+    kIOHIDEventFieldAccelerometerX = IOHIDEventFieldBase(kIOHIDEventTypeAccelerometer),
+    kIOHIDEventFieldAccelerometerY,
+    kIOHIDEventFieldAccelerometerZ,
+    kIOHIDEventFieldAccelerometerType,
+
+    // Mouse
+    kIOHIDEventFieldMouseX = IOHIDEventFieldBase(kIOHIDEventTypeMouse),
+    kIOHIDEventFieldMouseY,
+    kIOHIDEventFieldMouseZ,
+    kIOHIDEventFieldMouseButtonMask,
+    kIOHIDEventFieldMouseNumber,
+    kIOHIDEventFieldMouseClickCount,
+    kIOHIDEventFieldMousePressure,
+
+    // AmbientLightSensor
+    kIOHIDEventFieldAmbientLightSensorLevel = IOHIDEventFieldBase(kIOHIDEventTypeAmbientLightSensor),
+    kIOHIDEventFieldAmbientLightSensorRawChannel0,
+    kIOHIDEventFieldAmbientLightSensorRawChannel1,
+    kIOHIDEventFieldAmbientLightDisplayBrightnessChanged,
+
+    // Temperature
+    kIOHIDEventFieldTemperatureLevel = IOHIDEventFieldBase(kIOHIDEventTypeTemperature),
+
+    // Proximity
+    kIOHIDEventFieldProximityDetectionMask = IOHIDEventFieldBase(kIOHIDEventTypeProximity),
+
+    // Orientation
+    kIOHIDEventFieldOrientationRadius = IOHIDEventFieldBase(kIOHIDEventTypeOrientation),
+    kIOHIDEventFieldOrientationAzimuth,
+    kIOHIDEventFieldOrientationAltitude,
+
+    // Keyboard
+    kIOHIDEventFieldKeyboardUsagePage = IOHIDEventFieldBase(kIOHIDEventTypeKeyboard),
+    kIOHIDEventFieldKeyboardUsage,
+    kIOHIDEventFieldKeyboardDown,
+    kIOHIDEventFieldKeyboardRepeat,
+
+    // Digitizer
+    kIOHIDEventFieldDigitizerX = IOHIDEventFieldBase(kIOHIDEventTypeDigitizer),
+    kIOHIDEventFieldDigitizerY,
+    kIOHIDEventFieldDigitizerZ,
+    kIOHIDEventFieldDigitizerButtonMask,
+    kIOHIDEventFieldDigitizerType,
+    kIOHIDEventFieldDigitizerIndex,
+    kIOHIDEventFieldDigitizerIdentity,
+    kIOHIDEventFieldDigitizerEventMask,
+    kIOHIDEventFieldDigitizerRange,
+    kIOHIDEventFieldDigitizerTouch,
+    kIOHIDEventFieldDigitizerPressure,
+    kIOHIDEventFieldDigitizerAuxiliaryPressure, // BarrelPressure
+    kIOHIDEventFieldDigitizerTwist,
+    kIOHIDEventFieldDigitizerTiltX,
+    kIOHIDEventFieldDigitizerTiltY,
+    kIOHIDEventFieldDigitizerAltitude,
+    kIOHIDEventFieldDigitizerAzimuth,
+    kIOHIDEventFieldDigitizerQuality,
+    kIOHIDEventFieldDigitizerDensity,
+    kIOHIDEventFieldDigitizerIrregularity,
+    kIOHIDEventFieldDigitizerMajorRadius,
+    kIOHIDEventFieldDigitizerMinorRadius,
+    kIOHIDEventFieldDigitizerCollection,
+    kIOHIDEventFieldDigitizerCollectionChord,
+    kIOHIDEventFieldDigitizerChildEventMask,
+    kIOHIDEventFieldDigitizerIsDisplayIntegrated,
+    kIOHIDEventFieldDigitizerQualityRadiiAccuracy,
+    kIOHIDEventFieldDigitizerGenerationCount,
+    kIOHIDEventFieldDigitizerWillUpdateMask,
+    kIOHIDEventFieldDigitizerDidUpdateMask,
+
+    // Swipe
+    kIOHIDEventFieldSwipeMask = IOHIDEventFieldBase(kIOHIDEventTypeSwipe),
+
+    // Progress
+    kIOHIDEventFieldProgressEventType = IOHIDEventFieldBase(kIOHIDEventTypeProgress),
+    kIOHIDEventFieldProgressLevel
+};
+
+#endif /* IOHIDEventField_h */
